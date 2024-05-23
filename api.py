90b85f709f0468e5afd8ee88edd4cbf5ae23d9a2
jsnodees <202280134@psu.palawan.edu.ph>
2024-05-23 19:55:21 +0800

    added get_employee

--- api.py
+++ api.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "admin"
+app.config["MYSQL_DB"] = "company"
 
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
+mysql = MySQL(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/employees", methods=["GET"])
+def get_employees():
+    curs = mysql.connection.cursor()
+    query = """
+    select * from employee
+    """
+    curs.execute(query)
+    data = curs.fetchall()
+    curs.close()
+
+    return make_response(jsonify(data), 200)
 
 
 
